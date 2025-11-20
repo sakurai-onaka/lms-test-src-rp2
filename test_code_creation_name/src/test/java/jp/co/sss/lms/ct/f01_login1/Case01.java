67621832e3335788195a1be88bf60c79a0cf223e
sakurai hojo <h-sakurai@k-shared.co.jp>
2025-11-20 13:30:46 +0900

    add:Case02.java ログイン画面への遷移、 存在しないユーザーのログイン時エラーメッセージ出力テストコード追加 マジックバリューの追加 エビデンス取得
    fixCase01.java
    エビデンスファイル名修正

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -2,6 +2,7 @@ package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.ConstantTestValue.*;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,8 +13,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-
-import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -44,8 +43,10 @@ public class Case01 {
 	void test01() {
 		goTo(LMS_LOGIN_URL);
 		// 各画面表示時に10秒待機する
-		WebDriverUtils.webDriver.manage().timeouts().implicitlyWait(WAIT_TEN_SECOND, TimeUnit.SECONDS);
-		getEvidence(new Object() {});
+		webDriver.manage().timeouts().implicitlyWait(WAIT_TEN_SECOND, TimeUnit.SECONDS);
+		getEvidence(new Object() {},"transitionLogin");
+		String nowURL = webDriver.getCurrentUrl();
+		assertEquals(nowURL,LMS_LOGIN_URL);
 	}
 
 }
